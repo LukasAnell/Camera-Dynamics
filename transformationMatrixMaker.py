44b255220d304f14753bd7d642163f0c6fb42753
Lukas Anell <lukasmanell@gmail.com>
2025-03-20 13:49:51 -0700

    Works for rotating to the left!

--- transformationMatrixMaker.py
+++ transformationMatrixMaker.py
@@ -16,6 +16,20 @@ projectionPlaneDistanceFromCenter = 10 # δ
 # make the corner vectors
 
 def main():
+    # testMatrix = cv2.getPerspectiveTransform(
+    #     [(0, 0), (300, 0), (300, 300), (0, 300)],
+    #     [(0, 50), (300, 0), (300, 300), (0, 250)]
+    # )
+    # print(np.matrix(testMatrix))
+    # sourceImage = cv2.imread("test2.jpg")
+    # # convert transformationMatrix into a 2x3 matrix
+    # transformationMatrix = np.float32(np.reshape(testMatrix[:2], (2, 3)))
+    # imageTransformed = cv2.warpAffine(sourceImage, transformationMatrix, (sourceImage.shape[1], sourceImage.shape[0]))
+    # plt.imshow(cv2.cvtColor(imageTransformed, cv2.COLOR_BGR2RGB))
+    #
+    # plt.show()
+
+
     transformationMatrix = transformationMatrixMaker(cameraPosition, cameraForwardVector, cameraFocalHeight, cameraFocalLength, projectionPlaneDistanceFromCenter, (460, 341))
     print(np.matrix(transformationMatrix))
 
@@ -30,8 +44,13 @@ def main():
 
     sourceImage = cv2.imread("test2.jpg")
     # convert transformationMatrix into a 2x3 matrix
-    transformationMatrix = np.float32(np.reshape(transformationMatrix[:2], (2, 3)))
-    imageTransformed = cv2.warpAffine(sourceImage, transformationMatrix, (sourceImage.shape[1], sourceImage.shape[0]))
+    # transformationMatrix = np.float32(np.reshape(transformationMatrix[:2], (2, 3)))
+
+    # flip transformationMatrix along the y axis
+    # sourceImage = cv2.flip(sourceImage, 1)
+
+    imageTransformed = cv2.warpPerspective(sourceImage, transformationMatrix, (sourceImage.shape[1], sourceImage.shape[0]))
+
     plt.imshow(cv2.cvtColor(imageTransformed, cv2.COLOR_BGR2RGB))
 
     plt.show()
@@ -133,50 +152,107 @@ def transformationMatrixMaker(
 
     # translate all points in AB and CD so that they're centered at the origin
     # use image dimensions to scale the points
-
-    imageHeight = abs(finalCDPositions[2][1] - finalCDPositions[3][1])
-    imageResolutionHeight = imageDimensions[1] / imageHeight
-
-    for i in range(len(finalCDPositions)):
-        finalCDPositions[i][0] *= imageResolutionHeight
-        finalCDPositions[i][1] *= imageResolutionHeight
-
-    CDCenterX = finalCDPositions[1][0]
-    CDCenterY = finalCDPositions[3][1]
-
-    for i in range(len(finalCDPositions)):
-        finalCDPositions[i][0] -= CDCenterX
-        finalCDPositions[i][1] -= CDCenterY
-
+    #
+    # imageHeight = abs(finalCDPositions[2][1] - finalCDPositions[3][1])
+    # imageResolutionHeight = imageDimensions[1] / imageHeight
+    #
+    # for i in range(len(finalCDPositions)):
+    #     finalCDPositions[i][0] *= imageResolutionHeight
+    #     finalCDPositions[i][1] *= imageResolutionHeight
+    #
+    # CDCenterX = finalCDPositions[1][0]
+    # CDCenterY = finalCDPositions[3][1]
+    #
+    # for i in range(len(finalCDPositions)):
+    #     finalCDPositions[i][0] -= CDCenterX
+    #     finalCDPositions[i][1] -= CDCenterY
+    #
+    # # ABCenter = finalABPositions[3]
+    # # ABTopRightDestination = [finalABPositions[1][0] - ABCenter[0], finalABPositions[1][1] - ABCenter[1]]
+    # # xScalingFactor = finalCDPositions[1][0] / ABTopRightDestination[0]
+    # imageHeight = abs(finalABPositions[1][1] - finalABPositions[4][1])
+    # imageResolutionHeight = imageDimensions[1] / imageHeight
+    #
+    #
+    #
+    # for i in range(len(finalABPositions)):
+    #     finalABPositions[i] = [finalABPositions[i][0] * imageResolutionHeight, finalABPositions[i][1] * imageResolutionHeight]
+    #
     # ABCenter = finalABPositions[3]
-    # ABTopRightDestination = [finalABPositions[1][0] - ABCenter[0], finalABPositions[1][1] - ABCenter[1]]
-    # xScalingFactor = finalCDPositions[1][0] / ABTopRightDestination[0]
-    imageHeight = abs(finalABPositions[1][1] - finalABPositions[4][1])
-    imageResolutionHeight = imageDimensions[1] / imageHeight
+    # for i in range(len(finalABPositions)):
+    #     finalABPositions[i] = [finalABPositions[i][0] - ABCenter[0], finalABPositions[i][1] - ABCenter[1]]
 
+    # Find the minimum x and y values for finalABPositions
+    minXAB = min(pos[0] for pos in finalABPositions)
+    minYAB = min(pos[1] for pos in finalABPositions)
 
-
+    # Shift all coordinates in finalABPositions to be within the first quadrant
     for i in range(len(finalABPositions)):
-        finalABPositions[i] = [finalABPositions[i][0] * imageResolutionHeight, finalABPositions[i][1] * imageResolutionHeight]
+        finalABPositions[i][0] -= minXAB
+        finalABPositions[i][1] -= minYAB
 
-    ABCenter = finalABPositions[3]
+    # Find the minimum x and y values for finalCDPositions
+    minXCD = min(pos[0] for pos in finalCDPositions)
+    minYCD = min(pos[1] for pos in finalCDPositions)
+
+    # Shift all coordinates in finalCDPositions to be within the first quadrant
+    for i in range(len(finalCDPositions)):
+        finalCDPositions[i][0] -= minXCD
+        finalCDPositions[i][1] -= minYCD
+
+    # Calculate the scaling factors
+    imageWidth, imageHeight = imageDimensions
+    maxXAB = max(pos[0] for pos in finalABPositions)
+    maxYAB = max(pos[1] for pos in finalABPositions)
+    xScalingFactorAB = imageWidth / maxXAB
+    yScalingFactorAB = imageHeight / maxYAB
+
+    maxXCD = max(pos[0] for pos in finalCDPositions)
+    maxYCD = max(pos[1] for pos in finalCDPositions)
+    xScalingFactorCD = imageWidth / maxXCD
+    yScalingFactorCD = imageHeight / maxYCD
+
+    # Apply the scaling factors to each coordinate in finalABPositions
     for i in range(len(finalABPositions)):
-        finalABPositions[i] = [finalABPositions[i][0] - ABCenter[0], finalABPositions[i][1] - ABCenter[1]]
+        finalABPositions[i][0] *= xScalingFactorAB
+        finalABPositions[i][1] *= yScalingFactorAB
 
-    for point in finalABPositions[1:]:
-        print("(%.4f, %.4f)" % (point[0], point[1]))
-    print()
-    for point in finalCDPositions[1:]:
-        print("(%.4f, %.4f)" % (point[0], point[1]))
-    print()
+    # Apply the scaling factors to each coordinate in finalCDPositions
+    for i in range(len(finalCDPositions)):
+        finalCDPositions[i][0] *= xScalingFactorCD
+        finalCDPositions[i][1] *= yScalingFactorCD
+
+
+    # for point in finalABPositions[1:]:
+    #     print("(%.4f, %.4f)" % (point[0], point[1]))
+    # print()
+    # for point in finalCDPositions[1:]:
+    #     print("(%.4f, %.4f)" % (point[0], point[1]))
+    # print()
+
+
 
         # finalABPositions[i] = [x * (math.sqrt(projectionPlaneDistanceFromCenter**2 + CDCenter[0]**2 + CDCenter[1]**2) - 1) / (projectionPlaneDistanceFromCenter - 1) for x in finalABPositions[i]]
+
+    finalABPositions = [
+        finalABPositions[3],  # top-left
+        finalABPositions[1],  # top-right
+        finalABPositions[4],  # bottom-right
+        finalABPositions[2]  # bottom-left
+    ]
+
+    finalCDPositions = [
+        finalCDPositions[3],  # top-left
+        finalCDPositions[1],  # top-right
+        finalCDPositions[4],  # bottom-right
+        finalCDPositions[2]  # bottom-left
+    ]
 
     # use cv2.getPerspectiveTransform to get transformation matrix
     # AB is the source, CD is the destination
     # Ensure AB and CD are numpy arrays of type float32 and contain exactly 4 points
-    AB = np.array(finalABPositions[1:], dtype=np.float32)
-    CD = np.array(finalCDPositions[1:], dtype=np.float32)
+    AB = np.array(finalABPositions, dtype=np.float32)
+    CD = np.array(finalCDPositions, dtype=np.float32)
 
     transformationMatrix = cv2.getPerspectiveTransform(AB, CD)
     return transformationMatrix
